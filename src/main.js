16bc8c14b0b9f5b9dbb01d22042e12676ad89329
Rafael Camillo <camillodeveloper@gmail.com>
2023-05-24 11:24:00 -0300

    aff font awsome and fix sass

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+import '@fortawesome/fontawesome-free/css/all.css';
 
 Vue.use(Antd);
 Vue.config.productionTip = false;
